80f058565ac8f72d15557e6f9fbb6e75434dcca1
Sergey <darkus007@yandex.ru>
2023-04-16 00:05:09 +0300

    Добавлена возможность добавлять квартиры в избранное, страница пользователя с выбранными квартирами.

--- website/flats/urls.py
+++ website/flats/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls.static import static
 from django.urls import path, include
 
-from flats.views import IndexList, FlatDetailView, ProjectListView
+from flats.views import IndexList, FlatDetailView, ProjectListView, add_flat_to_selected, SelectedFlatList
 from website import settings
 
 urlpatterns = [
     path('', IndexList.as_view(), name='index'),
     path('flat/<int:flatid>/', FlatDetailView.as_view(), name='flat-detail'),
     path('project/<int:project_id>/', ProjectListView.as_view(), name='project-list'),
+    path('user-choice/<int:user_id>/', SelectedFlatList.as_view(), name='user-choice'),
+    path('select_flat/<int:flat_id>/', add_flat_to_selected, name='select-flat'),
 ]
 
 if settings.DEBUG:
--- website/flats/views.py
+++ website/flats/views.py
@@ -1,7 +1,12 @@
+from django.contrib import messages
+from django.db.models import Q, ObjectDoesNotExist, Subquery
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic import ListView, DetailView
 
-from flats.models import Flat, Price, AllFlatsLastPrice
+from .models import Flat, Price, AllFlatsLastPrice
+from members.models import SelectedFlat
 
 
 class IndexList(ListView):
@@ -12,6 +17,16 @@ class IndexList(ListView):
 
     def get_queryset(self):
         return AllFlatsLastPrice.objects.all()
+
+
+class SelectedFlatList(ListView):
+    model = Flat
+    template_name = 'flats/flats_selected_user.html'
+    context_object_name = 'flats'  # вместо objects_list
+
+    def get_queryset(self):
+        sq = Subquery(SelectedFlat.objects.filter(flats_user=self.kwargs['user_id']).values('flat_id'))
+        return AllFlatsLastPrice.objects.filter(flat_id__in=sq)
 
 
 class FlatDetailView(DetailView):
@@ -34,6 +49,13 @@ class FlatDetailView(DetailView):
             'price', 'booking_status',
             'benefit_name', 'benefit_description'
         ).filter(flat=self.kwargs['flatid']).order_by('-data_created')
+
+        total_selected = SelectedFlat.objects.filter(flat_id=self.kwargs['flatid']).count()
+        chose_this_user = SelectedFlat.objects.filter(Q(flats_user=self.request.user.id) &
+                                                      Q(flat_id=self.kwargs['flatid'])
+                                                      ).exists()    # True если нет False
+        context["total_selected"] = total_selected
+        context["chose"] = chose_this_user
         return context
 
 
@@ -49,3 +71,31 @@ class ProjectListView(ListView):
 
 def page_not_found(request, exception):
     return render(request, 'flats/base.html')
+
+
+def add_flat_to_selected(request, flat_id):
+    """
+    Добавляет квартиру в список отслеживаемых пользователем
+    или убирает из него, если она уже в нем.
+    """
+    try:
+        selected_flat = SelectedFlat.objects.get(flats_user=request.user.id, flat_id=flat_id)
+        selected_flat.delete()
+        messages.add_message(request, messages.SUCCESS, 'Квартира убрана из отслеживаемых.')
+        return HttpResponseRedirect(reverse('flat-detail', kwargs={'flatid': flat_id}))
+
+    except ObjectDoesNotExist:
+        user_selected_flats = SelectedFlat.objects.filter(flats_user=request.user)
+        if len(user_selected_flats) >= 3:
+            messages.add_message(request, messages.SUCCESS, 'Достигнут предел отслеживаемых квартир.')
+            return HttpResponseRedirect(reverse('flat-detail', kwargs={'flatid': flat_id}))
+
+        flat = Flat.objects.get(flat_id=request.POST.get('flat_id'))
+        data_created = Price.objects.values('data_created').get(flat=flat_id)
+        SelectedFlat.objects.create(
+            flats_user=request.user,
+            flat_id=flat,
+            data_created=data_created['data_created']
+        )
+        messages.add_message(request, messages.SUCCESS, 'Квартира добавлена к отслеживаемым.')
+        return HttpResponseRedirect(reverse('flat-detail', kwargs={'flatid': flat_id}))
